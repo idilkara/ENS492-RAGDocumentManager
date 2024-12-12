8e20965fddb92f91a85c56ae375e91a0e3ab0e6e
idil <idilkara5@gmail.com>
2024-12-12 22:03:29 +0300

    UI  changes

--- doc-manager-ui/src/pages/side-panel-options/sidepanelSettings.js
+++ doc-manager-ui/src/pages/side-panel-options/sidepanelSettings.js
@@ -10,21 +10,18 @@ const Settings = () => {
   
     return (
       <div className="settings-container">
-        <h2 className="settings-title">Settings</h2>
+    
         <div className="settings-options">
           <div className="settings-option">
           
-            <button className="dark-light-mode-toggle" >
-              Switch to {theme === 'Light' ? 'Dark' : 'Light'} Mode
-            </button>
           </div>
           <div className="settings-option">
 
-            <button className="feedback-button">Give Feedback</button>
+            <button className="settings-button">Give Feedback</button>
           </div>
           <div className="settings-option">
 
-            <button className="logout-button">Logout</button>
+            <button className="settings-button">Logout</button>
           </div>
         </div>
       </div>
