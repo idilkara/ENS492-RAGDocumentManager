d81c1422b6371f7f43473afbca0f12407d26ceac
idil <idilkara5@gmail.com>
2024-12-22 16:12:03 +0300

    run llama on port 11434 in a docker container

---
+++ Readme.md
@@ -0,0 +1,5 @@
+This is to run a llm server on port 30000 using docker
+
+there is a shell script for this you can modify or just run your model I dont care. 
+
+ollama-config.sh 
---
+++ ollama-config.sh
@@ -0,0 +1,8 @@
+
+
+#bashcript to run ollama on port 11434 
+docker pull ollama/ollama
+docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+docker exec -it ollama ollama run llama2
+
+
