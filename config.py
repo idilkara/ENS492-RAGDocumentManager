1d35ba2f227832c1d5ddb2e6dac1aa766a833fd6
iyelce <idilyelce18@gmail.com>
2025-02-18 00:55:00 +0300

    model switch2.0

--- config.py
+++ config.py
@@ -16,4 +16,6 @@ EMBEDDING_MODEL_NAME = "nomic-embed-text"
 
 LLAMA_MODEL_3_2_3B = "llama3.2:3b"
 LLAMA_MODEL_3_3_70B = "llama3.3:70b"
+DEEPSEEK_MODEL_R1_1_5B = "deepseek-r1:1.5b"
+MISTRAL_MODEL_7B = "mistral:7b"
 
--- model_state.py
+++ model_state.py
@@ -1,4 +1,4 @@
-from config import LLAMA_MODEL_3_2_3B, LLAMA_MODEL_3_3_70B
+from config import LLAMA_MODEL_3_2_3B, LLAMA_MODEL_3_3_70B, DEEPSEEK_MODEL_R1_1_5B, MISTRAL_MODEL_7B
 
 current_model = LLAMA_MODEL_3_2_3B  # default model
 
@@ -11,6 +11,10 @@ def set_current_model(model_name):
         current_model = LLAMA_MODEL_3_2_3B
     elif model_name.lower() == "llama3.3:70b":
         current_model = LLAMA_MODEL_3_3_70B
+    elif model_name.lower() == "deepseek-r1:1.5b":
+        current_model = DEEPSEEK_MODEL_R1_1_5B
+    elif model_name.lower() == "mistral:7b":
+        current_model = MISTRAL_MODEL_7B
     else:
         raise ValueError(f"Invalid model name.")
     return current_model 
