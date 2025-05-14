b293d93d73a6b351665a4e2539bdc33fa1bb548e
idil <idilkara5@gmail.com>
2025-05-14 15:09:56 +0300

    config

--- backend/backend/config.py
+++ backend/backend/config.py
@@ -10,12 +10,9 @@ SESSIONS_COLLECTION = os.getenv('SESSIONS_COLLECTION', 'sessions')
 # ChromaDB configuration
 CHROMADB_URL = os.getenv('CHROMADB_URL', 'http://chromadb:8000')  # Use the ChromaDB container name
 CHROMADB_DIR = "db"
-# Embedding Model Configuration
-EMBEDDING_MODEL_URL = os.getenv('EMBEDDING_MODEL_URL', 'http://10.3.0.96:8888/v1/')
 
-
+EMBEDDING_MODEL_URL = os.getenv('EMBEDDING_MODEL_URL', 'http://10.3.0.96:8888/v1/') #used to be ollama 
 EMBEDDING_MODEL_NAME = os.getenv('EMBEDDING_MODEL_NAME', 'nomic-embed-text')
-
 EMBEDDING_MODEL_NAME_V2 = "nomic-ai/nomic-embed-text-v2-moe"
 
 
@@ -27,3 +24,9 @@ LLAMA_MODEL_3_3_70B = "llama3.3:70b"
 DEEPSEEK_MODEL_R1_1_5B = "deepseek-r1:1.5b"
 MISTRAL_MODEL_7B = "mistral:7b"
 
+DEEPSEEK_TOKENIZER = "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B"
+DEEPSEEK_R1_MODELNAME = "DeepSeek-R1-Distill-Qwen-32B-Q6_K.ggu"
+
+LLM_MODEL_NAME = DEEPSEEK_R1_MODELNAME
+TOKENIZER_NAME = DEEPSEEK_TOKENIZER
+LLM_URI = "http://10.3.0.96:8888/v1"
