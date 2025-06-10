66550f78f1e5dcbcc52af31dd7d7a87e43fe3b21
umutkucukkayhan <umut.kucukkayhan@sabanciuniv.edu>
2025-06-10 22:28:42 +0300

    changed login system to CAS

--- frontend/doc-manager-ui/src/App.js
+++ frontend/doc-manager-ui/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/LoginPage'; // Import the Login page
 import Main from './pages/Main'; // Import the Main UI
+import CASLanding from './pages/CASLanding'; // Import the CAS Landing page
 import './root.css';
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} /> {/* Default route -> Login */}
+        <Route path="/cas-landing" element={<CASLanding />} />
         <Route path="/main" element={<Main />} /> {/* Chatbot UI */}
       </Routes>
     </Router>
